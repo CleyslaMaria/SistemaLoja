f1b32e27cfb4b745f888c8707499f2fc47a2200f
borgabe <shoebriel@academico.ufs.br>
2025-07-19 01:34:14 -0300

    Update PessoaFisica.java

--- Sistema/src/loja/model/cliente/PessoaFisica.java
+++ Sistema/src/loja/model/cliente/PessoaFisica.java
@@ -1,1 +1,18 @@
 package loja.model.cliente;
+
+public class PessoaFisica extends Cliente {
+    private String cpf;
+
+    public PessoaFisica(String id, String nome, String cpf) {
+        super(id, nome); 
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
