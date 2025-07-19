7943229ebf0cd3ecb2e9223d561d7e04aeee0ef8
Maria-meedragon <aparecida.nascimento@dcomp.ufs.br>
2025-07-19 19:41:31 -0300

    Update ItemNota.java

--- Sistema/src/loja/model/nota/ItemNota.java
+++ Sistema/src/loja/model/nota/ItemNota.java
@@ -1,1 +1,32 @@
 package loja.model.nota;
+// Emissão de Nota de Compra
+// Classe ItemNota (produto + quantidade)
+
+public class ItemNota {
+    private String nomeProduto;
+    private int quantidade;
+    private double precoUnidade;
+
+    public ItemNota(String nomeProduto, int quantidade, double precoUnidade) {
+        this.nomeProduto = nomeProduto;
+        this.quantidade = quantidade;
+        this.precoUnidade = precoUnidade;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+    
+    public double getPrecoUnidade() {
+        return precoUnidade;
+    }
+
+    public double getSubtotal() {
+        return quantidade * precoUnidade
+    }
+
+}
