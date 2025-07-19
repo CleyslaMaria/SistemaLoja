9df9e43df5dc14e9a0b402b3cbb052efe72de55a
borgabe <shoebriel@academico.ufs.br>
2025-07-19 01:35:15 -0300

    Update PessoaJuridica.java

--- Sistema/src/loja/model/cliente/PessoaJuridica.java
+++ Sistema/src/loja/model/cliente/PessoaJuridica.java
@@ -1,1 +1,18 @@
 package loja.model.cliente;
+
+public class PessoaJuridica extends Cliente {
+    private String cnpj;
+
+    public PessoaJuridica(String id, String nome, String cnpj) {
+        super(id, nome); 
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
