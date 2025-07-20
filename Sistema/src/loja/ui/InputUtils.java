a4a67e86f58a277d11cbc86c7a08c6279e5f9656
borgabe <shoebriel@academico.ufs.br>
2025-07-19 22:06:30 -0300

    Update InputUtils.java

--- Sistema/src/loja/ui/InputUtils.java
+++ Sistema/src/loja/ui/InputUtils.java
@@ -1,1 +1,23 @@
 package loja.ui;
+
+import java.util.Scanner;
+
+public class InputUtils {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int lerInt(String mensagem) {
+        System.out.print(mensagem);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Digite um número válido: ");
+            scanner.next(); 
+        }
+        return scanner.nextInt();
+    }
+
+    public static String lerString(String mensagem) {
+        System.out.print(mensagem);
+        return scanner.next();
+    }
+
+   
+}
