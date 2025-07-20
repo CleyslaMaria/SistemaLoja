82e8d58f5360c2c2bd0940c879d7542b9d1f6b90
Maria-meedragon <aparecida.nascimento@dcomp.ufs.br>
2025-07-19 22:10:11 -0300

    Update Nota.java

--- Sistema/src/loja/model/nota/Nota.java
+++ Sistema/src/loja/model/nota/Nota.java
@@ -2,16 +2,16 @@
 package loja.model.nota;
 import loja.model.cliente.Cliente;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.UUID;
+import java.util.ArrayList;  // Para armazenar os itens
+import java.util.List; 
+import java.util.UUID;      // Gera um ID único para a nota
 import java.math.BigDecimal;
 
 public class NotaCompra {
     private String idNota;
     private LocalDateTimeing data;
-    private Cliente cliente;
-    private List <ItemNota> itens; itens;
+    private Cliente cliente;                
+    private List <ItemNota> itens; itens;  // Lista de produtos comprados
 
     public NotaCompra(Cliente cliente) {
         this.idNota = UUID.randomUUID().toString();
@@ -20,11 +20,13 @@ public class NotaCompra {
         this.itens = new ArrayList<>();   
     }
 
+       // Adiciona um item à nota
     public void adicionarItem(String nomeProduto, int quantidade, BigDecimal precoUnidade) {
         ItemNota item = new ItemNota(nomeProduto, quantidade, precoUnidade);
         itens.add(item);
     }
 
+      // Soma os subtotais de todos os itens
     public BigDecimal calcularTotal() {
         BigDecimal total = BigDecimal.ZERO;
         for (ItemNota : itens) {
@@ -33,6 +35,7 @@ public class NotaCompra {
         return total;
     }
 
+    // Exibe um resumo da nota
     public void resumoNota() (
         System.out.println("===== NOTA DE COMPRA =====");
         System.out.println("ID: " + idNota);
